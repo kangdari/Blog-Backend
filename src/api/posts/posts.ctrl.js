49e718c23b7c659aa7059435695c917eab4ac3da
kangdari_ <ksh44820@naver.com>
2020-01-19 18:39:40 +0900

    post 데이터 생성 && 삭제

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,80 +1,57 @@
-let postId = 1;
-
-const posts = [
-    {
-        id: 1,
-        title: '제목',
-        body: '내용',
-    },
-];
+import Post from '../models/posts.js';
 
 // 포스트 작성
 // POST /api/posts/
-export const write = ctx => {
-    // REST API의 Request는 ctx.request.body에서 조회 가능
-    const { title, body } = ctx.request.body;
-    postId += 1;
-    const post = { id: postId, title, body };
-    posts.push(post);
-    ctx.body = post;
+export const write = async ctx => {
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags
+    })
+    try{
+        // DB에 저장
+        await post.save();
+        ctx.body = post;    
+    }catch(e) {
+        ctx.throw(500, e); // 서버 에러
+    }
 };
 
 // 포스트 목록 조회
 // GET /api/posts/
-export const list = ctx => {
-    ctx.body = posts;
+export const list = async ctx => {
+    try{
+        // 모델 인스턴스의 find() 함수롤 데이터 조회
+        // exec() 를 붙여줘야 서버에 쿼리 요청
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    }catch(e) {
+        ctx.throw(500, e);
+    }
 };
 
 // 특정 포스트 조회
 // GET /api/posts/:id
-export const read = ctx => {
+export const read = async ctx => {
     const { id } = ctx.params;
-    // 주어진 id 값으로 posts 배열에서 찾아내야 함.
-    // params에서 가져온 id 값은 string
-    const post = posts.find(post => post.id.toString() === id);
-
-    if (!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 없습니다.',
-        };
-        return;
+    try{
+        // id 값을 가진 데이터를 조회 시 findById() 함수 사용
+        const post = await Post.findById(id).exec();
+        if(!post){
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.body = post;
+    }catch(e) {
+        ctx.throw(500, e);
     }
-    ctx.body = post;
 };
 
 // 특정 포스트 제거
 // DELETE /api/posts/:id
-export const remove = ctx => {
-    const { id } = ctx.params;
-    // 삭제할 post의 index 값 찾기
-    const index = posts.findIndex(post => post.id.toString() === id);
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 없습니다.',
-        };
-        return;
-    }
-    posts.splice(index, 1);
-    ctx.status = 204; // No Connect
-};
+export const remove = ctx => {};
 
 // 특정 포스트 부분 수정
 // PATCH /api/posts/:id
-export const update = ctx =>{
-    const { id } = ctx.params;
-    const index = posts.findIndex(post => post.id.toString() === id);
-    if(index === -1){
-        ctx.status = 404;
-        ctx.body = {
-            message: 'post 없음'
-        }
-        return;
-    }
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body
-    };
-    ctx.body = posts[index];
-}
+export const update = ctx => {};
