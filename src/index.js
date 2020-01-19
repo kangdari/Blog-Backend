424b5d3e15270fdec85caea6270e53430ebc56b5
kangdari_ <ksh44820@naver.com>
2020-01-19 17:48:21 +0900

    MongoDB 설치 && mongoose 설치, 적용 && 환경변수 파일 생성

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-Router');
 const bodyParser = require('koa-bodyparser');
+const mongooes = require('mongoose');
 // api 모듈 불러옴
 const api = require('./api/index.js');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 생성
+const { PORT, MONGO_URL } = process.env;
+
+mongooes
+    .connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(e => {
+        console.log(e);
+    });
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +30,7 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, ()=>{
-    console.log('port 4000')
-})
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log(`port : ${port}`);
+});
